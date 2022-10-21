041c8b9d2d0ee1a7766d9918a900d9be5d4c96f0
Jon-Inge Heggstad <jon.heggstad@gmail.com>
2022-10-21 09:36:33 +0200

    update actions with npmrc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,7 @@ const config = {
             ({
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
+                    breadcrumbs: false,
                 },
                 blog: {
                     showReadingTime: true,
