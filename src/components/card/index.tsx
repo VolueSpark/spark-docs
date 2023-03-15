c283d1e46b0a05aae128607967c628315a13c80e
Jon-Inge Heggstad <jon.heggstad@gmail.com>
2023-03-15 15:50:13 +0100

    updated frontpage and some more minor font usage updates

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import Link from '@docusaurus/Link'
-import ArrowForward from '@site/static/icons/arrow_forward_ios.svg'
 
 import style from './card.module.css'
 
 type CardProps = {
+    icon: React.ReactNode
     title: string
     description: string
     url?: string
 }
 
-export default function Card({ title, description, url }: CardProps) {
+export default function Card({ icon, title, description, url }: CardProps) {
     return (
         <div className={style.card_link}>
             <Link href={url}>
                 <div className={style.card_link_flex}>
-                    <div>
-                        <h3 className={style.title}>{title}</h3>
-                        <p>{description}</p>
-                    </div>
-                    <ArrowForward />
+                    {icon}
+                    <h3 className={style.title}>{title}</h3>
+                    <p>{description}</p>
                 </div>
             </Link>
         </div>
