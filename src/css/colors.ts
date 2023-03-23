50828d16e35382ec834ae6eab8bfd61568d70ef3
Jon <jon.heggstad@gmail.com>
2023-03-23 13:20:55 +0100

    Hotfix/disable dark theme specification (#19)
    * temp disable dark theme on the specification subpage as theming has to be configured manually
    
    * requested changes

---
+++ src/css/colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+    primary: {
+        primaryLight: '#363C6E',
+        primaryDark: '#D0D0FF',
+    },
+    bg: {
+        bgLight: '#FDFEFC',
+        bgDark: '#1B1B1D',
+        bgActiveLight: '#F2F2F2',
+        bgActiveDark: '#272729',
+    },
+    text: {
+        textLight: '#FFFFFF',
+        textDark: '#000000',
+    },
+}
+
+export default Colors
